9113ac6e68c1b96e2c0438410a8a1bf5e2454963
lianggangqiang <gq.liang@qq.com>
2026-06-17 15:27:23 +0800

    Inject FocusCoordinator and reconcile lock on scene activation

--- bible-reader/bible_readerApp.swift
+++ bible-reader/bible_readerApp.swift
@@ -2,8 +2,6 @@
 //  bible_readerApp.swift
 //  bible-reader
 //
-//  Created by Corleone on 2026/6/17.
-//
 
 import SwiftUI
 import SwiftData
@@ -11,23 +9,34 @@ import SwiftData
 @main
 struct bible_readerApp: App {
     @State private var settings = ReadingSettings()
+    @Environment(\.scenePhase) private var scenePhase
+    private let sharedModelContainer: ModelContainer
+    @State private var focus: FocusCoordinator
 
-    var sharedModelContainer: ModelContainer = {
+    init() {
         let schema = Schema([LastReadPosition.self, Bookmark.self, Highlight.self, Note.self,
                              ReadingPlan.self, ReadingSession.self])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let container: ModelContainer
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            container = try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
-    }()
+        self.sharedModelContainer = container
+        _focus = State(initialValue: FocusCoordinator(
+            context: container.mainContext, lock: FamilyControlsLockController()))
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(settings)
+                .environment(focus)
                 .preferredColorScheme(settings.colorScheme.colorScheme)
+                .onChange(of: scenePhase) { _, phase in
+                    if phase == .active { focus.reconcile() }
+                }
         }
         .modelContainer(sharedModelContainer)
     }
